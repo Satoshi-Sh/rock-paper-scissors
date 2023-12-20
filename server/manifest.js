2021fb80bf656a9d376bb97a3dbcd918e22f4c63
satoshi-sh <satoss1108@gmail.com>
2023-12-20 16:21:41 -0600

    Updates

--- server/manifest.js
+++ server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.904f944d.js","app":"_app/immutable/entry/app.5593817c.js","imports":["_app/immutable/entry/start.904f944d.js","_app/immutable/chunks/scheduler.88568125.js","_app/immutable/chunks/singletons.4d930fb7.js","_app/immutable/entry/app.5593817c.js","_app/immutable/chunks/scheduler.88568125.js","_app/immutable/chunks/index.f733c480.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ba7a52e0.js","app":"_app/immutable/entry/app.d3422100.js","imports":["_app/immutable/entry/start.ba7a52e0.js","_app/immutable/chunks/scheduler.88568125.js","_app/immutable/chunks/singletons.09879075.js","_app/immutable/entry/app.d3422100.js","_app/immutable/chunks/scheduler.88568125.js","_app/immutable/chunks/index.f733c480.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- server/nodes/1.js
+++ server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.08c6e371.js","_app/immutable/chunks/scheduler.88568125.js","_app/immutable/chunks/index.f733c480.js","_app/immutable/chunks/singletons.4d930fb7.js"];
+export const imports = ["_app/immutable/nodes/1.5c7d612a.js","_app/immutable/chunks/scheduler.88568125.js","_app/immutable/chunks/index.f733c480.js","_app/immutable/chunks/singletons.09879075.js"];
 export const stylesheets = [];
 export const fonts = [];
